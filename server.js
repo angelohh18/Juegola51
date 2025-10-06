9f87d4a4327e04875576bd44d4870c706368fc0b
Angelo Hernandez Holguin <angelohh18@MacBook-Air-de-Angelo.local>
2025-10-06 10:24:01 +0100

    Fix: Eliminar imports de PostgreSQL que causaban error en Render
    - Removido require('pg') y configuración de Pool
    - El proyecto usa SQLite, no PostgreSQL
    - Esto soluciona el error 'Exited with status 1'

--- server.js
+++ server.js
@@ -4,14 +4,6 @@ const express = require('express');
 const http = require('http');
 const { Server } = require("socket.io");
 const path = require('path');
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
 
 const app = express();
 const server = http.createServer(app);
